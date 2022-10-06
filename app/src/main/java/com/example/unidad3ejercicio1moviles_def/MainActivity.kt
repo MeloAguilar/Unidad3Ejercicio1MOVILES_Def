0edc7c4038d14a8679f99b705a6472103a72a86e
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-10-06 10:14:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/unidad3ejercicio1moviles_def/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.unidad3ejercicio1moviles_def
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
